7feebd8852b66dfaa5408d89fbe884fbd02ed52e
soham <s.jadiya@supraoracles.com>
2024-11-22 16:23:53 +0530

    updating the script with mainnet rclone

--- scripts/onboarding_mainnet_7_1_7.sh
+++ scripts/onboarding_mainnet_7_1_7.sh
@@ -997,11 +997,11 @@ snapshot_download(){
     while [ $retry_count -lt $MAX_RETRIES ]; do
         # Run the rclone sync command, output to the console and log simultaneously
         echo "Running rclone sync attempt $((retry_count + 1)) at $(date)" | tee -a "$LOG_FILE"
-        rclone sync cloudflare-r2:mainnet/snapshots/store "$HOST_SUPRA_HOME/smr_storage/" --progress | tee -a "$LOG_FILE"
+        rclone sync cloudflare-r2-mainnet:mainnet/snapshots/store "$HOST_SUPRA_HOME/smr_storage/" --progress | tee -a "$LOG_FILE"
 
         # Check if the rclone command was successful
         if [ $? -eq 0 ]; then
-            rclone sync cloudflare-r2:mainnet/snapshots/store "$HOST_SUPRA_HOME/smr_storage/" --progress | tee -a "$LOG_FILE"
+            rclone sync cloudflare-r2-mainnet:mainnet/snapshots/store "$HOST_SUPRA_HOME/smr_storage/" --progress | tee -a "$LOG_FILE"
             echo "rclone sync completed successfully at $(date)" | tee -a "$LOG_FILE"
             break
         else
